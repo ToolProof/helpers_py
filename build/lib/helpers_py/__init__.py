b22a516271faefaf2eb789402b710d5b742f5f5d
René Stavnes <renestavnes@hotmail.com>
2025-07-15 21:48:16 +0200

    Update repository URL to match renamed repo: helpers_py

---
+++ build/lib/helpers_py/__init__.py
@@ -0,0 +1,8 @@
+'''
+helpers_py: Utility functions for Google Cloud Storage operations.
+'''
+
+from .gcs_utils import download_from_gcs, upload_to_gcs
+
+__version__ = '0.1.0'
+__all__ = ['download_from_gcs', 'upload_to_gcs']
